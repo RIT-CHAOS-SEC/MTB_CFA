9b615b3767dab8d446dbf26fb2fadd0b36236ebc
Antonio Joia Neto <a.joia.neto@gmail.com>
2024-10-09 17:06:36 -0400

    update

--- IOTKit_CM33_S_NS/IOTKit_CM33_ns/Objects/led_v2m-mps2.d
+++ IOTKit_CM33_S_NS/IOTKit_CM33_ns/Objects/led_v2m-mps2.d
@@ -1,6 +1,6 @@
 ./objects/led_v2m-mps2.o: \
   C:\Users\aj4775\AppData\Local\arm\packs\Keil\V2M-MPS2_IOTKit_BSP\1.5.2\Boards\ARM\V2M-MPS2\Common\LED_V2M-MPS2.c \
-  RTE\_FVP\RTE_Components.h \
+  RTE\_V2M-MPS2_\RTE_Components.h \
   C:\Users\aj4775\AppData\Local\arm\packs\Keil\V2M-MPS2_IOTKit_BSP\1.5.2\Device\IOTKit_CM33\Include\IOTKit_CM33_FP.h \
   C:\Users\aj4775\AppData\Local\arm\packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\core_cm33.h \
   C:\Users\aj4775\AppData\Local\arm\packs\Keil\V2M-MPS2_IOTKit_BSP\1.5.2\Device\IOTKit_CM33\Include\system_IOTKit_CM33.h \
--- IOTKit_CM33_S_NS/IOTKit_CM33_s/main_s.c
+++ IOTKit_CM33_S_NS/IOTKit_CM33_s/main_s.c
@@ -3,25 +3,28 @@
  * Purpose: Main function secure mode
  *----------------------------------------------------------------------------*/
 
+#include "RTE_Components.h" /* Component selection */
 #include <arm_cmse.h>
+#include <stdint.h>
 #include <stdio.h>
-#include "RTE_Components.h"                        /* Component selection */
+
 #include CMSIS_device_header
-#include "Board_LED.h"                             /* ::Board Support:LED */
-#include "Board_GLCD.h"                            /* ::Board Support:Graphic LCD */
-#include "GLCD_Config.h"                           /* Keil.V2M-MPS2 IOT-Kit::Board Support:Graphic LCD */
+#include "Board_GLCD.h"  /* ::Board Support:Graphic LCD */
+#include "Board_LED.h"   /* ::Board Support:LED */
+#include "GLCD_Config.h" /* Keil.V2M-MPS2 IOT-Kit::Board Support:Graphic LCD */
 #include "mtb.h"
 
 /* Start address of non-secure application */
 #define NONSECURE_START (0x00200000u)
 
-extern GLCD_FONT     GLCD_Font_16x24;
+extern GLCD_FONT GLCD_Font_16x24;
 
-extern int stdout_init (void);
+extern int stdout_init(void);
 
 /* typedef for NonSecure callback functions */
-typedef int32_t (*NonSecure_fpParam)(uint32_t) __attribute__((cmse_nonsecure_call));
-typedef void    (*NonSecure_fpVoid) (void)     __attribute__((cmse_nonsecure_call));
+typedef int32_t (*NonSecure_fpParam)(uint32_t)
+    __attribute__((cmse_nonsecure_call));
+typedef void (*NonSecure_fpVoid)(void) __attribute__((cmse_nonsecure_call));
 
 char text[] = "Hello World (secure)\r\n";
 
@@ -29,207 +32,235 @@ char text[] = "Hello World (secure)\r\n";
   NonSecure callback functions
  *----------------------------------------------------------------------------*/
 extern NonSecure_fpParam pfNonSecure_LED_On;
-       NonSecure_fpParam pfNonSecure_LED_On  = (NonSecure_fpParam)NULL;
+NonSecure_fpParam pfNonSecure_LED_On = (NonSecure_fpParam)NULL;
 extern NonSecure_fpParam pfNonSecure_LED_Off;
-       NonSecure_fpParam pfNonSecure_LED_Off = (NonSecure_fpParam)NULL;
-
+NonSecure_fpParam pfNonSecure_LED_Off = (NonSecure_fpParam)NULL;
 
 /*----------------------------------------------------------------------------
   Secure functions exported to NonSecure application
  *----------------------------------------------------------------------------*/
-int32_t Secure_LED_On (uint32_t num) __attribute__((cmse_nonsecure_entry));
-int32_t Secure_LED_On (uint32_t num)
-{
-  return LED_On(num);
-}
+int32_t Secure_LED_On(uint32_t num) __attribute__((cmse_nonsecure_entry));
+int32_t Secure_LED_On(uint32_t num) { return LED_On(num); }
 
-int32_t Secure_LED_Off (uint32_t num) __attribute__((cmse_nonsecure_entry)) ;
-int32_t Secure_LED_Off (uint32_t num)
-{
-  return LED_Off(num);
-}
+int32_t Secure_LED_Off(uint32_t num) __attribute__((cmse_nonsecure_entry));
+int32_t Secure_LED_Off(uint32_t num) { return LED_Off(num); }
 
-void Secure_printf (char* pString) __attribute__((cmse_nonsecure_entry)) ;
-void Secure_printf (char* pString)
-{
-  printf("%s", pString);
-}
-
+void Secure_printf(char *pString) __attribute__((cmse_nonsecure_entry));
+void Secure_printf(char *pString) { printf("%s", pString); }
 
 /*----------------------------------------------------------------------------
   Secure function for NonSecure callbacks exported to NonSecure application
  *----------------------------------------------------------------------------*/
-int32_t Secure_LED_On_callback(NonSecure_fpParam callback) __attribute__((cmse_nonsecure_entry));
+int32_t Secure_LED_On_callback(NonSecure_fpParam callback)
+    __attribute__((cmse_nonsecure_entry));
 int32_t Secure_LED_On_callback(NonSecure_fpParam callback)
 {
-  pfNonSecure_LED_On = callback;
-  return 0;
+    pfNonSecure_LED_On = callback;
+    return 0;
 }
 
-int32_t Secure_LED_Off_callback(NonSecure_fpParam callback) __attribute__((cmse_nonsecure_entry));
+int32_t Secure_LED_Off_callback(NonSecure_fpParam callback)
+    __attribute__((cmse_nonsecure_entry));
 int32_t Secure_LED_Off_callback(NonSecure_fpParam callback)
 {
-  pfNonSecure_LED_Off = callback;
-  return 0;
+    pfNonSecure_LED_Off = callback;
+    return 0;
 }
-
 
 /*----------------------------------------------------------------------------
   SysTick IRQ Handler
  *----------------------------------------------------------------------------*/
-void SysTick_Handler (void);
-void SysTick_Handler (void) {
-  static uint32_t ticks = 0;
-  static uint32_t ticks_printf = 0;
+void SysTick_Handler(void);
+void SysTick_Handler(void)
+{
+    static uint32_t ticks = 0;
+    static uint32_t ticks_printf = 0;
 
-  switch (ticks++) {
-    case  10:
-      LED_On (0u);
-      break;
+    switch (ticks++)
+    {
+    case 10:
+        LED_On(0u);
+        break;
     case 20:
-      LED_Off(0u);
-      break;
+        LED_Off(0u);
+        break;
     case 30:
-      if (pfNonSecure_LED_On != NULL)
-      {
-        pfNonSecure_LED_On(1u);
-      }
-      break;
+        if (pfNonSecure_LED_On != NULL)
+        {
+            pfNonSecure_LED_On(1u);
+        }
+        break;
     case 50:
-      if (pfNonSecure_LED_Off != NULL)
-      {
-        pfNonSecure_LED_Off(1u);
-      }
-      break;
+        if (pfNonSecure_LED_Off != NULL)
+        {
+            pfNonSecure_LED_Off(1u);
+        }
+        break;
     case 99:
-      ticks = 0;
-      if (ticks_printf++ == 3)
-      {
-        printf("%s", text);
-        ticks_printf = 0;
-      }
-      break;
-    default:
-      if (ticks > 99) {
         ticks = 0;
-      }
-  }
+        if (ticks_printf++ == 3)
+        {
+            printf("%s", text);
+            ticks_printf = 0;
+        }
+        break;
+    default:
+        if (ticks > 99)
+        {
+            ticks = 0;
+        }
+    }
+}
+
+
+
+void entry_point(){
+  return;
+}
+
+void matmul()
+{
+    int mat[5][5];
+    int val = 0;
+    if (val == 1)
+    {
+        val++;
+    }
+    else
+    {
+        val += 2;
+    }
+
+    for (int x = 0; x < 2; x++)
+    {
+        for (int y = 0; y < 2; x++)
+        {
+            val += mat[x][y] + mat[y][x];
+        }
+    }
+    val = val + 2;
+    return;
+}
+
+
+void exit_point(void){
+  entry_point();
+  matmul();
+}
+
+void run(void){
+	exit_point();
 }
 
 
 
 
- void __attribute__((used)) matmul(){
-	int mat [5][5];
-	int val = 0;	
-	 
-	if (val==1){
-		val++;
-	}
-	else{
-		val +=2;
-	}
-	 
-	for (int x=0; x<5; x++){
-		for (int y=0; y<5; x++){
-			val += mat[x][y] + mat[y][x];
-		}
-	}
-	val = val+2;
-	return;
+MTB_struct *mtb = (MTB_struct *)MTB_BASE_addr;
+DWT_struct *dwt = (DWT_struct *)DWT_BASE_addr;
+
+#define DWT_FUNCTION_ACTION_VALUE 0b10 << DWT_FUNCTION_ACTION_OFFSET       // Generate a data trace match
+#define DWT_FUNCTION_DATAVSIZE_VALUE 0b10 << DWT_FUNCTION_DATAVSIZE_OFFSET // word size
+#define DWT_FUNCTION_MATCH_VALUE 0b0011 << DWT_FUNCTION_MATCH_OFFSET       // Generate a match on the data value
+
+#define DWT_FUNCTION_MODIFY_MASK (DWT_FUNCTION_MATCH_OFFSET | DWT_FUNCTION_DATAVSIZE_OFFSET | DWT_FUNCTION_ACTION_OFFSET)
+#define DWT_FUNCTION_MODIFY_VALUE (DWT_FUNCTION_MATCH_VALUE | DWT_FUNCTION_DATAVSIZE_VALUE | DWT_FUNCTION_ACTION_VALUE)
+
+void setup_DWT()
+{
+		// START SIGNAL
+    dwt->DWT_FUNCTION0[0] &= ~DWT_FUNCTION_MATCH_OFFSET; // disable DWT+CMP0
+    dwt->DWT_COMP0[0] = (uint32_t) entry_point;  // Initial Address
+    
+    dwt->DWT_FUNCTION1[0] = (dwt->DWT_FUNCTION0[0] & ~DWT_FUNCTION_MODIFY_MASK) | DWT_FUNCTION_MODIFY_VALUE;
+    dwt->DWT_COMP1[0] = (uint32_t) exit_point; // Final Address
+	
+	
+		// STOP SIGNAL
+	  dwt->DWT_FUNCTION2[0] &= ~DWT_FUNCTION_MATCH_OFFSET; // disable DWT+CMP2
+    dwt->DWT_COMP2[0] = (uint32_t) run;  // Initial Address
+    
+    dwt->DWT_FUNCTION3[0] = (dwt->DWT_FUNCTION0[0] & ~DWT_FUNCTION_MODIFY_MASK) | DWT_FUNCTION_MODIFY_VALUE;
+    dwt->DWT_COMP3[0] = (uint32_t) setup_DWT; // Final Address
+	
+    return;
 }
 
-
-MTB_struct * mtb = (MTB_struct*) MTB_BASE_addr;
-
-uint32_t mtb_buff[128] __attribute__((aligned(64)));
-
-
-void setup_MTB(){
+void setup_MTB()
+{
+		mtb->MTB_TSTART |= 0b10;  // Set to use DWT_COMP1
 	
-	mtb->MTB_BASE = (uint32_t) mtb_buff & MTB_BASE_MASK;
-	mtb->MTB_FLOW =  0;
-	mtb->MTB_POSITION = 0;
+		mtb->MTB_TSTOP  |= 0b1000;  // Set to use DWT_COMP3
+		
+    mtb->MTB_FLOW = 0;
+    mtb->MTB_POSITION = 0;
 
-	mtb->MTB_MASTER = MTB_MASTER_EN_MASK | 
-									  MTB_MASTER_MASK_MASK|
-										MTB_MASTER_TSTARTEN_MASK	;
-	
-	matmul();
-	
-	return;
-	
-	
+    mtb->MTB_MASTER = MTB_MASTER_EN_MASK | MTB_MASTER_MASK_MASK | MTB_MASTER_TSTARTEN_MASK;
+
+    matmul();
+
+    return;
 }
-	
-
-
 
 static uint32_t x;
 /*----------------------------------------------------------------------------
   Main function
  *----------------------------------------------------------------------------*/
-int main (void)
+int main(void)
 {
-  uint32_t         NonSecure_StackPointer =                   (*((uint32_t *)(NONSECURE_START + 0u)));
-  NonSecure_fpVoid NonSecure_ResetHandler = (NonSecure_fpVoid)(*((uint32_t *)(NONSECURE_START + 4u)));
+    uint32_t NonSecure_StackPointer = (*((uint32_t *)(NONSECURE_START + 0u)));
+    NonSecure_fpVoid NonSecure_ResetHandler =
+        (NonSecure_fpVoid)(*((uint32_t *)(NONSECURE_START + 4u)));
 
-  /* exercise some floating point instructions from Secure Mode */
-  volatile uint32_t fpuType = SCB_GetFPUType(); 
-  volatile float  x1 = 12.4567f;
-  volatile float  x2 = 0.6637967f;
-  volatile float  x3 = 24.1111118f;
+    /* exercise some floating point instructions from Secure Mode */
+    volatile uint32_t fpuType = SCB_GetFPUType();
+    volatile float x1 = 12.4567f;
+    volatile float x2 = 0.6637967f;
+    volatile float x3 = 24.1111118f;
 
-  x3 = x3 * (x1 / x2);
+    x3 = x3 * (x1 / x2);
 
-  /* exercise some core register from Secure Mode */
-  x = __get_MSP();
-  x = __get_PSP();
-  __TZ_set_MSP_NS(NonSecure_StackPointer);
-  x = __TZ_get_MSP_NS();
-  __TZ_set_PSP_NS(0x22000000u);
-  x = __TZ_get_PSP_NS();
+    /* exercise some core register from Secure Mode */
+    x = __get_MSP();
+    x = __get_PSP();
+    __TZ_set_MSP_NS(NonSecure_StackPointer);
+    x = __TZ_get_MSP_NS();
+    __TZ_set_PSP_NS(0x22000000u);
+    x = __TZ_get_PSP_NS();
 
-  SystemCoreClockUpdate();
+    SystemCoreClockUpdate();
 
-  stdout_init();                          /* Initialize Serial interface */
-  LED_Initialize ();
-  GLCD_Initialize();
+    stdout_init(); /* Initialize Serial interface */
+    LED_Initialize();
+    GLCD_Initialize();
 
-  /* display initial screen */
-  GLCD_SetFont(&GLCD_Font_16x24);
-  GLCD_SetBackgroundColor(GLCD_COLOR_WHITE);
-  GLCD_ClearScreen();
-  GLCD_SetBackgroundColor(GLCD_COLOR_BLUE);
-  GLCD_SetForegroundColor(GLCD_COLOR_RED);
-  GLCD_DrawString(0*16, 0*24, "   V2M-MPS2+ Demo   ");
-  GLCD_DrawString(0*16, 1*24, " Secure/Non-Secure  ");
-  GLCD_DrawString(0*16, 2*24, "   www.keil.com     ");
+    /* display initial screen */
+    GLCD_SetFont(&GLCD_Font_16x24);
+    GLCD_SetBackgroundColor(GLCD_COLOR_WHITE);
+    GLCD_ClearScreen();
+    GLCD_SetBackgroundColor(GLCD_COLOR_BLUE);
+    GLCD_SetForegroundColor(GLCD_COLOR_RED);
+    GLCD_DrawString(0 * 16, 0 * 24, "   V2M-MPS2+ Demo   ");
+    GLCD_DrawString(0 * 16, 1 * 24, " Secure/Non-Secure  ");
+    GLCD_DrawString(0 * 16, 2 * 24, "   www.keil.com     ");
 
-  GLCD_SetBackgroundColor(GLCD_COLOR_WHITE);
-  GLCD_SetForegroundColor(GLCD_COLOR_BLACK);
-  switch ((SCB->CPUID >> 4) & 0xFFF) {
+    GLCD_SetBackgroundColor(GLCD_COLOR_WHITE);
+    GLCD_SetForegroundColor(GLCD_COLOR_BLACK);
+    switch ((SCB->CPUID >> 4) & 0xFFF)
+    {
     case 0xD20:
-      GLCD_DrawString(0*16, 4*24, "  Cortex-M23        ");
-      break;
+        GLCD_DrawString(0 * 16, 4 * 24, "  Cortex-M23        ");
+        break;
     case 0xD21:
-      GLCD_DrawString(0*16, 4*24, "  Cortex-M33        ");
-      break;
+        GLCD_DrawString(0 * 16, 4 * 24, "  Cortex-M33        ");
+        break;
     default:
-      GLCD_DrawString(0*16, 4*24, "  unknown Cortex-M  ");
-      break;
-  }
-	
-	
-	
+        GLCD_DrawString(0 * 16, 4 * 24, "  unknown Cortex-M  ");
+        break;
+    }
 
-  SysTick_Config(SystemCoreClock / 100);  /* Generate interrupt each 10 ms */
+    SysTick_Config(SystemCoreClock / 100); /* Generate interrupt each 10 ms */
 
-	setup_MTB();
-	
-  NonSecure_ResetHandler();
+    setup_MTB();
 
-	
-
+    NonSecure_ResetHandler();
 }
-
